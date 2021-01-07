d4e94c0e7c928a6450a6e8e1d01755f9cdaa7b21
mnjoyanx <tigranmnjoyan@gmail.com>
2021-01-07 12:10:25 +0400

    first commit

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
-        <h2>this is register page</h2>
+    <div class="auth-page">
+        <div class="container-page">
+            <div class="row">
+                <div class="col-md-6 offset-md-3 col-xs-12">
+                    <h2 class="text-xs-center">Sign Up</h2>
+                    <p class="text-xs-center">
+                        <router-link :to={name: 'signUp'}>Need an account?</router-link>
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
